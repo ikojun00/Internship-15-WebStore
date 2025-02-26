9c365101a0154a406e433a436bded98f1cb9ee95
Ivor <ikojun00@fesb.hr>
2025-02-26 12:55:04 +0100

    implement fakestore api

---
+++ src/services/api.ts
@@ -0,0 +1,18 @@
+import { Product } from "../types/Product";
+
+const API_URL = "https://fakestoreapi.com";
+
+export const fetchProducts = async (): Promise<Product[]> => {
+  const response = await fetch(`${API_URL}/products`);
+  return response.json();
+};
+
+export const fetchProduct = async (id: number): Promise<Product> => {
+  const response = await fetch(`${API_URL}/products/${id}`);
+  return response.json();
+};
+
+export const fetchCategories = async (): Promise<string[]> => {
+  const response = await fetch(`${API_URL}/products/categories`);
+  return response.json();
+};
